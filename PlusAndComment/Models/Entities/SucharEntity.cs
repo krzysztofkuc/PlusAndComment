63da89b9feb66b81b2417bb94326bb3dce7098d0
Krzysiek-PC\Krzysiek <krzysztofkuc@email.com>
2017-08-08 10:13:09 +0200

    Dodane sucharz 2

---
+++ PlusAndComment/Models/Entities/SucharEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PlusAndComment.Models.Entities
+{
+    [Table("Suchary")]
+    public class SucharEntity
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string AbsThumbPath { get; set; }
+        public string RelThumbPath { get; set; }
+        public DateTime AddedTime { get; set; }
+    }
+}
---
+++ PlusAndComment/Models/SucharVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PlusAndComment.Models
+{
+	public class SucharVM
+	{
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string AbsThumbPath { get; set; }
+        public string RelThumbPath { get; set; }
+        public DateTime AddedTime { get; set; }
+    }
+}
